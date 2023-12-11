1089e90524e8f10a16105b25b6cb365a86762049
Tim Parker <parker_tim@yahoo.co.uk>
2023-12-11 17:34:22 +0100

    Day 12 prep

---
+++ 12/main.js
@@ -0,0 +1,12 @@
+utils = require('../utils.js')
+
+data = utils.fileToArray("input.txt")
+        .map(line => line.split(""))
+
+console.log(data)
+
+console.log(`Part 1: ${""}`)
+
+
+
+console.log(`Part 2: ${""}`)
