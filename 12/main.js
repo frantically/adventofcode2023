016551110a07763a2740843e4bec08fe38100359
frantically <tim@frantically.org>
2023-12-12 06:10:24 +0000

    Temp

--- 12/main.js
+++ 12/main.js
@@ -1,12 +1,30 @@
 utils = require('../utils.js')
 
-data = utils.fileToArray("input.txt")
+data = utils.fileToArray("sample.txt")
         .map(line => line.split(""))
 
 console.log(data)
 
 console.log(`Part 1: ${""}`)
 
+const characters = ["a", "b", "c", "d"];
 
+function combinations(arr, min = 1, max) {
+  const combination = (arr, depth) => {
+    if (depth === 1) {
+      return arr;
+    } else {
+      const result = combination(arr, depth - 1).flatMap((val) =>
+        arr.map((char) => val + char)
+      );
+      return arr.concat(result);
+    }
+  };
+
+  return combination(arr, max).filter((val) => val.length >= min);
+}
+const result = combinations(characters, 4, 4);
+
+//console.log(`Combinations: ${result.length}`, result);
 
 console.log(`Part 2: ${""}`)
