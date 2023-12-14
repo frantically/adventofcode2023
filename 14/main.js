955b8ef24f8f1896fdc232e5e72c95ed718c12f2
Tim Parker <parker_tim@yahoo.co.uk>
2023-12-14 06:13:23 +0100

    Prep

--- 14/main.js
+++ 14/main.js
@@ -5,6 +5,25 @@ utils = require('../utils.js')
 data = utils.fileToArray("sample.txt")
     .map(line => line.split(""))
 
+console.log(data)
+
+movedARock = true
+
+while(movedARock) {
+    movedARock = false
+    for(i=0;i<data.length-1;i++) {
+        for(j=0;j<data[0].length;j++) {
+            if(data[i][j] == "." && data[i+1][j] == "O") {
+                data[i][j] = data[i+1][j]
+                data[i+1][j] = "."
+                movedARock = true
+            }
+        }
+    }
+}
+
+data.forEach(row => console.log(row.join("")))
+
 part1 = ""
 
 console.log(`Part 1: ${part1}`)
